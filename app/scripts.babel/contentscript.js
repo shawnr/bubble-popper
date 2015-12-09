d9fc0375e4046d08d3e729b480ae0bdd25cbf37a
Shawn Rider <shawn@shawnrider.com>
2015-12-08 19:59:46 -0800

    Implemented working regex matching.
    Change-Id: I00426819d1fee056a022842ae5ad41e9eccbe81f

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -4,56 +4,57 @@ console.log('Altered! Content script');
 
 var bubbleTerms = [
     {
-        'a': 'Death Tax',
-        'b': 'Estate Tax',
+        'a': new RegExp(/(Death Tax)/gim),
+        'b': new RegExp(/(Estate Tax)/gim),
         'descriptionA': 'Term used by fiscal conservatives for estate tax.',
         'descriptionB': 'Also known as "inheritance tax."'
     },
     {
-        'a': 'Affordable Care Act',
-        'b': 'Obamacare',
+        'a': new RegExp(/(Affordable Care Act)/gim),
+        'b': new RegExp(/(Obamacare)/gim),
         'descriptionA': 'Act signed by President Obama to reform health care industry in 2010',
         'descriptionB': 'Term used often by fiscally conservative Republican party members (as opposed to Democratic) to talk about the Affordable Care Act'
     },
     {
-        'a': '\b(Climate Change)\W+(?:\w+\W+){1,}?(scam)\b',
-        'b': 'Climate Change',
+        //'a': '\b(Climate Change)[\W+(?:\w+\W+){1,}]*(scam)\b',
+        'a': new RegExp(/(Climate Change)[\W+(?:\w+\W+){1,}]*(scam)/gim),
+        'b': new RegExp(/(Climate Change)/gim),
         'descriptionA': 'These words are often used together by climate change deniers ',
         'descriptionB': 'Designates the theory that the earth\'s atmosphere is in a steady rise due to human production of Co2'
     },
     {
-        'a': 'Evolution + Creation',
-        'b': 'Evolution',
+        'a': new RegExp(/(Evolution)[\W+(?:\w+\W+){1,}]*(Creation)/gim),
+        'b': new RegExp(/(Evolution)/gim),
         'descriptionA': 'These words are often used together by Creationists and intellegent design theorists, arguing for god\'s hand in origin of species.',
         'descriptionB': 'Designates the theory that species desended slowly over millions of years from a shared ancestor. '
     },
     {
-        'a': 'Vaccination + Autism',
-        'b': 'Vaccination',
+        'a': new RegExp(/(Vaccination)[\W+(?:\w+\W+){1,}]*(Autism)/gim),
+        'b': new RegExp(/(Vaccination)/gim),
         'descriptionA': 'These words are often used together by vaccination skeptics who believe a link between vaccines and autism exists.',
         'descriptionB': 'Designates the practice of introducing an agent into the body to immunize it against disease.'
     },
     {
-        'a': 'Ammendment + Self-Defense + Arms',
-        'b': 'People + Gun Control',
+        'a': new RegExp(/(Ammendment)[\W+(?:\w+\W+){1,}]*(Arms)/gim),
+        'b': new RegExp(/(People)[\W+(?:\w+\W+){1,}]*(Gun Control)/gim),
         'descriptionA': 'These words often appear in the discourses of gun rights advocates',
         'descriptionB': 'These words often appear in the discourses of gun control advocates'
     },
     {
-        'a': 'Death Penalty + Murder',
-        'b': 'Death Penalty + Capital Punishment + Deterrence',
+        'a': new RegExp(/(Death Penalty)[\W+(?:\w+\W+){1,}]*(Murder)/gim),
+        'b': new RegExp(/(Death Penalty)[\W+(?:\w+\W+){1,}]*(Capital Punishment)/gim),
         'descriptionA': 'These words appear often in the discourses of those in opposition to the death penalty',
         'descriptionB': 'These words often appear in the discourses of those in support of the death penalty'
     },
     {
-        'a': 'Pro-Life',
-        'b': 'Pro-Choice',
+        'a': new RegExp(/(Pro-Life)/gim),
+        'b': new RegExp(/(Pro-Choice)/gim),
         'descriptionA': 'Term used by anti-abortionists to designate the choice of mothers',
         'descriptionB': 'Term used by pro-abortionists to designate the choice of mothers'
     },
     {
-        'a': 'Homosexualism',
-        'b': 'Gay',
+        'a': new RegExp(/(Homosexualism)/gim),
+        'b': new RegExp(/(Gay)/gim),
         'descriptionA': 'Term used often by anti-gay advocates',
         'descriptionB': 'Term often used over the term homosexual by those sympathetic to gay rights'
     }
@@ -68,42 +69,26 @@ bubbleTerms.forEach(function(term){
     var result = {};
     var matched = false;
     console.log('checking: ' + term.a);
-    if (bodyText.includes(term.a) || bodyText.includes(term.a.toLowerCase())){
+
+    var matchArray = term.a.test(bodyText);
+    if (matchArray !== false) {
+        matched = true;
+        console.log('matchArray = ' + matchArray);
         result = {
             term: term.a,
             description: term.descriptionA
         }
-        matched = true;
-    } else {
-        var bubbleRE = new RegExp(term.a);
-        var matchArray = bubbleRE.exec(bodyText);
-        if (matchArray != null) {
-            matched = true;
-            console.log('matchArray = ' + matchArray);
-            result = {
-                term: term.a,
-                description: term.descriptionA
-            }
-        }
     }
+
 
     if (!matched){
         console.log('checking: ' + term.b);
-        if (bodyText.includes(term.b) || bodyText.includes(term.b.toLowerCase())) {
-            result = {
-                term: term.b,
-                description: term.descriptionB
-            }
+        var matchArray = term.b.test(bodyText);
+        if (matchArray !== false) {
             matched = true;
-        } else {
-            var bubbleRE = new RegExp(term.b);
-            var matchArray = bubbleRE.exec(bodyText);
-            if (matchArray != null) {
-                matched = true;
-                result = {
-                    term: term.a,
-                    description: term.descriptionA
-                }
+            result = {
+                term: term.a,
+                description: term.descriptionA
             }
         }
     }
